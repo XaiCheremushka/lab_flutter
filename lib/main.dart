69b9838e91054f1f91762190ecca23ff0c7645a7
XaiCheremushka <vania55ru@mail.ru>
2023-11-30 23:57:13 +0600

    Tariffs and limits

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 void main() {
   runApp(const MyApp());
@@ -77,7 +78,8 @@ class MyApp extends StatelessWidget {
                             ),
                           ),
                         ),
-                        Container(width: 187.50, height: 19),
+                        Container(width: 187.50, height: 5),
+                        Image.asset("assets/images/Active.png", height: 2, width: 187.5,)
                       ],
                     ),
 
@@ -99,14 +101,170 @@ class MyApp extends StatelessWidget {
                               ),
                             ),
                         ),
-                        Container(width: 187.50, height: 19),
+                        Container(width: 187.50, height: 7),
                       ],
                     )
                   ],
+                ),
+              ),
+              Container(
+                color: Color.fromARGB(1, 250, 250, 250),
+                child:  Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child: Row(
+                          children: [
+                            Text(
+                              'У вас подключено',
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                fontFamily: 'SF Pro Text',
+                                fontWeight: FontWeight.w700,
+                                height: 1,
+                                letterSpacing: -0.70,
+                              ),
+                            ),
+                          ]
+                      ),
+                    ),
+
+                    const Padding(
+                      padding: EdgeInsets.only(top: 8),
+                      child: Row(
+                        children: [
+                          Flexible(
+                            child: Text(
+                              'Подписки, автоплатежи и сервисы, на которые вы подписались',
+                              style: TextStyle(
+                                color: Colors.black26,
+                                fontSize: 14,
+                                fontFamily: 'SF Pro Text',
+                                fontWeight: FontWeight.w500,
+                                height: 1,
+                                letterSpacing: -0.42,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                  const Padding(
+                    padding: EdgeInsets.only(top: 46),
+                    child: Row(
+                      children: [
+                        Text(
+                          'Тарифы и лимиты',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontFamily: 'SF Pro Text',
+                            fontWeight: FontWeight.w700,
+                            height: 1,
+                            letterSpacing: -0.70,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                    const Padding(
+                      padding: EdgeInsets.only(top: 8),
+                      child: Row(
+                        children: [
+                          Text(
+                            'Для операций в Сбербанк Онлайн',
+                            style: TextStyle(
+                              color: Colors.black26,
+                              fontSize: 14,
+                              fontFamily: 'SF Pro Text',
+                              fontWeight: FontWeight.w500,
+                              height: 1,
+                              letterSpacing: -0.42,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+
+                    Padding(
+                      padding: EdgeInsets.only(top: 26),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Image.asset(
+                              "assets/images/ic_36_speedometer.png",
+                              alignment: Alignment.centerLeft,
+                              height: 36,
+                                // width: 36+12
+                              ),
+                          ),
+
+                          const Expanded(
+                            flex: 4,
+                            child: Column(
+                          children: [
+                          Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Изменить суточный лимит',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontFamily: 'SF Pro Text',
+                                  fontWeight: FontWeight.w500,
+                                  height: 1,
+                                  letterSpacing: -0.40,
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                'На платежи и переводы',
+                                style: TextStyle(
+                                  color: Colors.black26,
+                                  fontSize: 14,
+                                  fontFamily: 'SF Pro Text',
+                                  fontWeight: FontWeight.w500,
+                                  height: 1,
+                                  letterSpacing: -0.41,
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                          ),
+
+                          Expanded(
+                            flex: 1,
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: Image.asset(
+                                "assets/images/Disclosure.png",
+                                alignment: Alignment.centerRight,
+                                height: 24,
+                                // width: 24
+                              ),
+                            )
+
+                          ),
+                        ],
+                      ),
+                    ),
+
+                  ],
                 )
               )
-
-
             ],
           ),
         ),
